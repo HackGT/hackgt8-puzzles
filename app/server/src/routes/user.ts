8933b9dae0ef74765f0c2f6467a1fad5cfb19c6e
Anish Thite <anishthite@gmail.com>
2021-07-11 18:22:21 -0400

    status route

--- app/server/src/routes/user.ts
+++ app/server/src/routes/user.ts
@@ -1,9 +1,26 @@
 import express from "express";
-
+import passport from "passport";
+import { createNoSubstitutionTemplateLiteral } from "typescript";
+import {User, IUser} from '../entity/User';
 export let userRoutes = express.Router();
 
 userRoutes.route("/status").get(async (req, res) => {
-        return res.status(200).send("todo");
+       
+        const uuid = req.user?.['uuid'];
+
+        console.log(uuid);
+        if (!uuid){
+                return res.send({success: false, data: "uuid not specified"})
+
+        }
+
+        let user = await User.findOne({uuid: uuid}); 
+
+        if (!user) {
+                return res.send({success: false, data: "user not found"})
+        }
+
+        return res.send({success: true, data: {user}})
 });
 
   
